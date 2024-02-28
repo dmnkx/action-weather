ee7f363c22410423210a901d6ccc9e6642c54534
Dominick <dominick@cookat.co>
2024-02-28 18:14:55 +0900

    Initial

---
+++ index.js
@@ -0,0 +1,15 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+
+try {
+	const key = core.getInput('key');
+	console.log(`Access Key: ${key}`);
+
+	const content = "Hello, World";
+	core.setOutput("content", content);
+
+	const payload = JSON.stringify(github.context.payload, undefined, 2);
+	console.log(`The event payload: ${payload}`);
+} catch (error) {
+	core.setFailed(error.message);
+}
